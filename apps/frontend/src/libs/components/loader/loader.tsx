528bbdab73db887767463c04cdeaa36822678686
Dima Bohachuk <dima.b.02@ukr.net>
2025-07-21 22:39:00 +0300

    fix(frontend): apply CSS variable for loader color ss-19

--- apps/frontend/src/libs/components/loader/loader.tsx
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -1,6 +1,7 @@
 import { type JSX } from "react";
 
 import Spinner from "./spinner.svg?react";
+import styles from "./styles.module.css";
 
 type Properties = {
 	sizeInPx?: number;
@@ -11,7 +12,9 @@ const AVERAGE_LOADER_SIZE_IN_PX = 32;
 const Loader = ({
 	sizeInPx = AVERAGE_LOADER_SIZE_IN_PX,
 }: Properties): JSX.Element => {
-	return <Spinner height={sizeInPx} width={sizeInPx} />;
+	return (
+		<Spinner className={styles["loader"]} height={sizeInPx} width={sizeInPx} />
+	);
 };
 
 export { Loader };

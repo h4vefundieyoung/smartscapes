cdc1c4933f46986c8df972414a170e6cee73102e
Dima Bohachuk <dima.b.02@ukr.net>
2025-07-22 20:20:40 +0300

    feat(backend): add groups and permissions tables and seeds ss-49

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -1,5 +1,8 @@
 const DatabaseTableName = {
+	GROUP_TO_PERMISSIONS: "group_to_permissions",
+	GROUPS: "groups",
 	MIGRATIONS: "migrations",
+	PERMISSIONS: "permissions",
 	USERS: "users",
 } as const;
 

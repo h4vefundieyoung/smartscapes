3c2ce3fc6ab7497508f34df92d305f1ce9244a4c
Vyacheslav Khoroshev <vyacheslavkhoroshev.gmail.com>
2025-07-22 16:48:26 +0300

    feat(backend): add poi migration ss-5

---
+++ apps/backend/src/db/migrations/20250722133759_add_poi_table.ts
@@ -0,0 +1,20 @@
+import { type Knex } from "knex";
+
+const TABLE_NAME = "points_of_interest";
+
+function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable(TABLE_NAME, (table) => {
+		table.increments("id").primary();
+		table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+		table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+		table.string("name").notNullable();
+		table.float("longitude").notNullable();
+		table.float("latitude").notNullable();
+	});
+}
+
+export { down, up };

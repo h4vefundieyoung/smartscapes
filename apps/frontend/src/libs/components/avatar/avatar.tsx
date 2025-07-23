20d6e19765338ebe0210f05f13a5a64b10be5eaa
vldkorin <korinvlad2@gmail.com>
2025-07-23 14:12:43 +0300

    feat(frontend): add avatar component ss-21

---
+++ apps/frontend/src/libs/components/avatar/avatar.tsx
@@ -0,0 +1,41 @@
+import { type JSX } from "react";
+
+import { Image } from "../components.js";
+import styles from "./styles.module.css";
+
+type Properties = {
+	user: User;
+};
+
+type User = {
+	avatarUrl: null | string;
+	firstName: string;
+	lastName: string;
+};
+
+const getInitials = (user: User): string => {
+	const firstChar = user.firstName[0];
+	const lastChar = user.lastName[0];
+
+	const firstNameInitial = firstChar ? firstChar.toUpperCase() : "";
+	const lastNameInitial = lastChar ? lastChar.toUpperCase() : "";
+
+	return `${firstNameInitial}${lastNameInitial}`;
+};
+
+const Avatar = ({ user }: Properties): JSX.Element => {
+	return (
+		<div className={styles["avatar"]}>
+			<Image
+				alt="User Avatar"
+				className={styles["avatarImage"] ?? ""}
+				fallback={
+					<span className={styles["fallback"]}>{getInitials(user)}</span>
+				}
+				src={user.avatarUrl ?? ""}
+			/>
+		</div>
+	);
+};
+
+export { Avatar };

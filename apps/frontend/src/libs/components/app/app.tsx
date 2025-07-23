590334494502399857eb51c98a46608b4c0b3a1e
vldkorin <korinvlad2@gmail.com>
2025-07-23 18:43:43 +0300

    chore(frontend): update app component ss-14

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,6 +1,17 @@
-import { Loader, RouterOutlet } from "~/libs/components/components.js";
+import reactLogo from "~/assets/images/react.svg";
+import {
+	Button,
+	Link,
+	Loader,
+	RouterOutlet,
+} from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { useAppDispatch, useEffect, useLocation } from "~/libs/hooks/hooks.js";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useEffect,
+	useLocation,
+} from "~/libs/hooks/hooks.js";
 import { actions as userActions } from "~/modules/users/users.js";
 
 import styles from "./styles.module.css";
@@ -8,6 +19,8 @@ import styles from "./styles.module.css";
 const App = (): React.JSX.Element => {
 	const { pathname } = useLocation();
 	const dispatch = useAppDispatch();
+	const dataStatus = useAppSelector(({ users }) => users.dataStatus);
+	const users = useAppSelector(({ users }) => users.data);
 
 	const isRoot = pathname === AppRoute.ROOT;
 
@@ -19,10 +32,36 @@ const App = (): React.JSX.Element => {
 
 	return (
 		<div className={styles["container"]}>
-			<Loader />
+			<img alt="logo" className="App-logo" src={reactLogo} width="30" />
+			<ul className="App-navigation-list">
+				<li>
+					<Link to={AppRoute.ROOT}>Root</Link>
+				</li>
+				<li>
+					<Link to={AppRoute.SIGN_IN}>Sign in</Link>
+				</li>
+				<li>
+					<Link to={AppRoute.SIGN_UP}>Sign up</Link>
+				</li>
+			</ul>
+			<p>Current path: {pathname}</p>
+
 			<div>
 				<RouterOutlet />
 			</div>
+			{isRoot && (
+				<>
+					<p>State: {dataStatus}</p>
+					<Loader />
+					<h3>Users:</h3>
+					<ul>
+						{users.map((user) => (
+							<li key={user.id}>{user.email}</li>
+						))}
+					</ul>
+					<Button label="Button for test" type="button" />
+				</>
+			)}
 		</div>
 	);
 };

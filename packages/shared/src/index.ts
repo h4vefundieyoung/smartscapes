6ccd97898150d299145488645c7a448124f93623
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-07-23 18:05:10 +0300

    fix(shared): add HTTP type export to shared module ss-52

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -8,6 +8,7 @@ export { ValidationError } from "./libs/exceptions/exceptions.js";
 export { configureString } from "./libs/helpers/helpers.js";
 export { type Config } from "./libs/modules/config/config.js";
 export {
+	type HTTP,
 	HTTPCode,
 	HTTPError,
 	HTTPHeader,

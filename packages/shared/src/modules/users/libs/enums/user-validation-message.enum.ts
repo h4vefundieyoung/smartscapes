a6a171daf59c990fd00b2797212f842ba851d719
vldkorin <korinvlad2@gmail.com>
2025-07-23 14:56:42 +0300

    chore(shared): delete unnecessary message ss-14

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -5,7 +5,6 @@ const UserValidationMessage = {
 	EMAIL_WRONG: "Email format is wrong.",
 	FIRST_NAME_REQUIRED: "First name is required.",
 	LAST_NAME_REQUIRED: "Last name is required.",
-	NAME_REQUIRED: "Name is required.",
 	PASSWORD_MINIMUM_LENGTH: `Password must be at least ${String(UserValidationRule.PASSWORD_MINIMUM_LENGTH)} characters long.`,
 } as const;
 

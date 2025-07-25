d1e658f0a1110b751dc3f8558f889491b44e5dad
whispermind <h4vefun.dieyoung@gmail.com>
2025-07-25 14:02:22 +0300

    feat(backend): inject user entity into controller handler ss-23

--- apps/backend/src/libs/modules/controller/libs/types/api-handler-options.type.ts
+++ apps/backend/src/libs/modules/controller/libs/types/api-handler-options.type.ts
@@ -1,15 +1,19 @@
+import { type UserGetAllItemResponseDto } from "@smartscapes/shared";
+
 type APIHandlerOptions<
 	HandlerOptions extends DefaultApiHandlerOptions = DefaultApiHandlerOptions,
 > = {
 	body: HandlerOptions["body"];
 	params: HandlerOptions["params"];
 	query: HandlerOptions["query"];
+	user?: HandlerOptions["user"];
 };
 
 type DefaultApiHandlerOptions = {
 	body?: unknown;
 	params?: unknown;
 	query?: unknown;
+	user?: UserGetAllItemResponseDto;
 };
 
 export { type APIHandlerOptions };

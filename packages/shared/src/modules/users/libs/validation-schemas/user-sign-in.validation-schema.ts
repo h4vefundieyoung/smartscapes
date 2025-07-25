c962c486b66e4372525563be617304a424163c07
Mykhailo Derkach <my.derkach@gmail.com>
2025-07-25 14:46:01 +0200

    feat(shared): add maximum length limitation to sign in validation ss-17

--- packages/shared/src/modules/users/libs/validation-schemas/user-sign-in.validation-schema.ts
+++ packages/shared/src/modules/users/libs/validation-schemas/user-sign-in.validation-schema.ts
@@ -11,10 +11,19 @@ const userSignIn: ZodType<UserSignInRequestDto> = z.strictObject({
 		.trim()
 		.min(UserValidationRule.EMAIL_MINIMUM_LENGTH, {
 			error: UserValidationMessage.EMAIL_REQUIRED,
+		})
+		.max(UserValidationRule.MAX_LENGTH, {
+			error: UserValidationMessage.EMAIL_MAXIMUM_LENGTH,
 		}),
-	password: z.string().trim().min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
-		error: UserValidationMessage.PASSWORD_MINIMUM_LENGTH,
-	}),
+	password: z
+		.string()
+		.trim()
+		.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
+			error: UserValidationMessage.PASSWORD_MINIMUM_LENGTH,
+		})
+		.max(UserValidationRule.MAX_LENGTH, {
+			error: UserValidationMessage.PASSWORD_MAXIMUM_LENGTH,
+		}),
 });
 
 export { userSignIn };

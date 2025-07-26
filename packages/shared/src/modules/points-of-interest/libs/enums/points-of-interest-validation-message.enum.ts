cefaded848793062faed027e173bf9cd84cd6781
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-07-26 09:45:06 +0300

    refactor(backend/shared): simplify return statements and improve validation message handling ss-52

--- packages/shared/src/modules/points-of-interest/libs/enums/points-of-interest-validation-message.enum.ts
+++ packages/shared/src/modules/points-of-interest/libs/enums/points-of-interest-validation-message.enum.ts
@@ -3,6 +3,7 @@ import { PointsOfInterestValidationRule } from "./points-of-interest-validation-
 const PointsOfInterestValidationMessage = {
 	NAME_MAXIMUM_LENGTH: `Name must be less than ${String(PointsOfInterestValidationRule.NAME_MAXIMUM_LENGTH)} characters long.`,
 	NAME_MINIMUM_LENGTH: `Name must be at least ${String(PointsOfInterestValidationRule.NAME_MINIMUM_LENGTH)} characters long.`,
+	REQUIRED_FIELDS_FOR_UPDATE: "At least one field must be provided for update.",
 } as const;
 
 export { PointsOfInterestValidationMessage };

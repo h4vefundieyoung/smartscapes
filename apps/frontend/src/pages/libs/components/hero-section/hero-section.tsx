51f21cc671772e5810e01794fd1c67935ce852c5
= <=>
2025-07-27 17:10:15 +0300

    style(frontend): fix styles names and image container ss-53

--- apps/frontend/src/pages/libs/components/hero-section/hero-section.tsx
+++ apps/frontend/src/pages/libs/components/hero-section/hero-section.tsx
@@ -2,21 +2,22 @@ import styles from "./styles.module.css";
 
 const HeroSection = (): React.JSX.Element => {
 	return (
-		<section className={styles["hero-section"]}>
+		<section className={styles["section"]}>
 			<div>
-				<h1 className={styles["hero-section-title"]}>
+				<h1 className={styles["section-title"]}>
 					Discover Your Next Adventure
 				</h1>
 
-				<p className={styles["hero-section-description"]}>
+				<p className={styles["section-description"]}>
 					Find the best trails for hiking, biking, and exploring the outdoors
 					wherever you are.
 				</p>
 			</div>
 
-			<div className={styles["hero-section-cover"]}>
+			<div className={styles["section-cover"]}>
 				<img
 					alt="Landing - forest view"
+					className={styles["section-cover-image"]}
 					src="/assets/images/landing-cover.jpg"
 				/>
 			</div>

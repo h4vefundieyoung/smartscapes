7dc52c747d5ce14927de113f5c36fefa616f4cac
vldkorin <korinvlad2@gmail.com>
2025-07-27 20:50:47 +0300

    refactor(backend/shared): add constraint, foreign keys, remove validation from service ss-75

--- apps/backend/src/modules/reviews/review.service.ts
+++ apps/backend/src/modules/reviews/review.service.ts
@@ -1,10 +1,7 @@
-import { HTTPCode } from "~/libs/modules/http/http.js";
 import { type CollectionResult, type Service } from "~/libs/types/types.js";
 import { ReviewEntity } from "~/modules/reviews/review.entity.js";
 import { type ReviewRepository } from "~/modules/reviews/review.repository.js";
 
-import { ReviewExceptionMessage } from "./libs/enums/enums.js";
-import { ReviewError } from "./libs/exceptions/exceptions.js";
 import {
 	type ReviewRequestDto,
 	type ReviewResponseDto,
@@ -18,20 +15,6 @@ class ReviewService implements Service {
 	}
 
 	public async create(payload: ReviewRequestDto): Promise<ReviewResponseDto> {
-		if (!(payload.routeId || payload.poiId)) {
-			throw new ReviewError({
-				message: ReviewExceptionMessage.ROUTE_OR_POI_REQUIRED,
-				status: HTTPCode.BAD_REQUEST,
-			});
-		}
-
-		if (payload.routeId && payload.poiId) {
-			throw new ReviewError({
-				message: ReviewExceptionMessage.ROUTE_OR_POI_REQUIRED,
-				status: HTTPCode.BAD_REQUEST,
-			});
-		}
-
 		const reviewEntity = ReviewEntity.initializeNew({
 			content: payload.content,
 			likesCount: 0,

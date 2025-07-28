77e6b4d75eb4e9f2d8c98035d445950086cf11ee
Max Kotyk <mxm.kotyk@gmail.com>
2025-07-28 16:16:57 +0200

    refactor(backend): update not found exception message ss-76

--- packages/shared/src/modules/route-categories/libs/enums/route-category-exception-message.enum.ts
+++ packages/shared/src/modules/route-categories/libs/enums/route-category-exception-message.enum.ts
@@ -1,6 +1,6 @@
 const RouteCategoryExceptionMessage = {
 	ALREADY_EXISTS: "Route category already exists",
-	NOT_FOUND: "Not Found",
+	NOT_FOUND: "Such category doesn't exist",
 } as const;
 
 export { RouteCategoryExceptionMessage };

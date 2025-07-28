2e10f068a204a5de09d0112256e6bfce4338c220
vldkorin <korinvlad2@gmail.com>
2025-07-29 00:34:28 +0300

    fix(backend): solve conflict ss-75

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -2,8 +2,8 @@ const DatabaseTableName = {
 	GROUPS: "groups",
 	GROUPS_TO_PERMISSIONS: "groups_to_permissions",
 	MIGRATIONS: "migrations",
-	REVIEWS: "reviews",
 	PERMISSIONS: "permissions",
+	REVIEWS: "reviews",
 	USERS: "users",
 } as const;
 

e19b88a4b63d8f35d877a650562709d07f255431
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-07-28 10:07:08 +0300

    fix(backend): remove unnecessary type annotation for validation schema ss-52

--- packages/shared/src/modules/points-of-interest/libs/validation-schemas/point-of-interest-create.validation-schema.ts
+++ packages/shared/src/modules/points-of-interest/libs/validation-schemas/point-of-interest-create.validation-schema.ts
@@ -1,21 +1,19 @@
-import { z, type ZodType } from "zod";
+import { z } from "zod";
 
 import {
 	PointsOfInterestValidationMessage,
 	PointsOfInterestValidationRule,
 } from "../enums/enums.js";
-import { type PointsOfInterestRequestDto } from "../types/types.js";
 
-const pointOfInterestCreate: ZodType<PointsOfInterestRequestDto> =
-	z.strictObject({
-		name: z
-			.string()
-			.min(PointsOfInterestValidationRule.NAME_MINIMUM_LENGTH, {
-				message: PointsOfInterestValidationMessage.NAME_MINIMUM_LENGTH,
-			})
-			.max(PointsOfInterestValidationRule.NAME_MAXIMUM_LENGTH, {
-				message: PointsOfInterestValidationMessage.NAME_MAXIMUM_LENGTH,
-			}),
-	});
+const pointOfInterestCreate = z.strictObject({
+	name: z
+		.string()
+		.min(PointsOfInterestValidationRule.NAME_MINIMUM_LENGTH, {
+			message: PointsOfInterestValidationMessage.NAME_MINIMUM_LENGTH,
+		})
+		.max(PointsOfInterestValidationRule.NAME_MAXIMUM_LENGTH, {
+			message: PointsOfInterestValidationMessage.NAME_MAXIMUM_LENGTH,
+		}),
+});
 
 export { pointOfInterestCreate };

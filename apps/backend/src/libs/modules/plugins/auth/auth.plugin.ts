b3f62be3edb602b464c2d61016dd1cf936b995c1
whispermind <h4vefun.dieyoung@gmail.com>
2025-07-28 20:04:00 +0300

    refactor(backend): move error handling into upper scope ss-23

--- apps/backend/src/libs/modules/plugins/auth/auth.plugin.ts
+++ apps/backend/src/libs/modules/plugins/auth/auth.plugin.ts
@@ -1,12 +1,6 @@
-import {
-	type FastifyInstance,
-	type FastifyReply,
-	type FastifyRequest,
-} from "fastify";
+import { type FastifyInstance, type FastifyRequest } from "fastify";
 import fastifyPlugin from "fastify-plugin";
-import { JOSEError } from "jose/errors";
 
-import { HTTPCode, HTTPError } from "~/libs/modules/http/http.js";
 import { type HTTPMethod } from "~/libs/types/types.js";
 import { userService } from "~/modules/users/users.js";
 
@@ -30,42 +24,29 @@ const auth = (
 	app: FastifyInstance,
 	{ whiteRoutesList }: PluginOptions,
 ): void => {
-	const requestHandler = async (
-		request: FastifyRequest,
-		reply: FastifyReply,
-	): Promise<void> => {
+	const requestHandler = async (request: FastifyRequest): Promise<void> => {
 		const { headers, method, url } = request;
 
-		try {
-			const isWhiteListRoute = whiteRoutesList.some(
-				({ method: _method, path }) => url.endsWith(path) && _method === method,
-			);
+		const isWhiteListRoute = whiteRoutesList.some(
+			({ method: _method, path }) => url.endsWith(path) && _method === method,
+		);
 
-			if (isWhiteListRoute) {
-				return;
-			}
+		if (isWhiteListRoute) {
+			return;
+		}
 
-			if (!headers.authorization) {
-				throw new AuthError();
-			}
+		if (!headers.authorization) {
+			throw new AuthError();
+		}
 
-			const [, token] = headers.authorization.split(" ");
-			const { userId } = await tokenService.verify<TokenPayload>(
-				token as string,
-			);
-			const user = await userService.findById(userId);
+		const [, token] = headers.authorization.split(" ");
+		const { userId } = await tokenService.verify<TokenPayload>(token as string);
+		const user = await userService.findById(userId);
 
-			if (user) {
-				request.user = user;
-			} else {
-				throw new AuthError();
-			}
-		} catch (error) {
-			let status =
-				error instanceof HTTPError || error instanceof JOSEError
-					? HTTPCode.UNAUTHORIZED
-					: HTTPCode.INTERNAL_SERVER_ERROR;
-			reply.status(status).send(error);
+		if (user) {
+			request.user = user;
+		} else {
+			throw new AuthError();
 		}
 	};
 

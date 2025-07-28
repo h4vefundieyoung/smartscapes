01987589f0bbc98f961943f878c75d1bea7b51cb
Mykhailo Derkach <my.derkach@gmail.com>
2025-07-28 15:23:36 +0200

    feat(shared): add notification validation message ss-75

--- packages/shared/src/modules/notifications/libs/enums/enums.ts
+++ packages/shared/src/modules/notifications/libs/enums/enums.ts
@@ -2,4 +2,5 @@ export { EntityType } from "./entity-type.enum.js";
 export { NotificationApiPath } from "./notification-api-path.enum.js";
 export { NotificationExceptionMessage } from "./notification-exception-message.enum.js";
 export { NotificationType } from "./notification-type.enum.js";
+export { NotificationValidationMessage } from "./notification-validation-message.enum.js";
 export { NotificationValidationRule } from "./notification-validation-rule.enum.js";

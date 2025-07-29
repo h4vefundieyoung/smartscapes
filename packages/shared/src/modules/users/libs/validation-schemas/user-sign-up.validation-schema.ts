0491aa1cfd65181e16ef5e02e150c8e0cc33ab58
Mykhailo Derkach <my.derkach@gmail.com>
2025-07-29 21:58:21 +0200

    fix(shared): update validation schems for password, first and last names ss-110

--- packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
+++ packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
@@ -7,9 +7,6 @@ const userSignUp = z
 		confirmPassword: z
 			.string()
 			.trim()
-			.nonempty({
-				error: UserValidationMessage.PASSWORD_REQUIRED,
-			})
 			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
 				error: UserValidationMessage.PASSWORD_MINIMUM_LENGTH,
 			}),
@@ -24,27 +21,18 @@ const userSignUp = z
 		firstName: z
 			.string()
 			.trim()
-			.nonempty({
-				error: UserValidationMessage.FIRST_NAME_REQUIRED,
-			})
 			.min(UserValidationRule.FIRST_NAME_MINIMUM_LENGTH, {
-				error: UserValidationMessage.FIRST_NAME_MINIMUM_LENGTH,
+				error: UserValidationMessage.FIRST_NAME_REQUIRED,
 			}),
 		lastName: z
 			.string()
 			.trim()
-			.nonempty({
-				error: UserValidationMessage.LAST_NAME_REQUIRED,
-			})
 			.min(UserValidationRule.LAST_NAME_MINIMUM_LENGTH, {
-				error: UserValidationMessage.LAST_NAME_MINIMUM_LENGTH,
+				error: UserValidationMessage.LAST_NAME_REQUIRED,
 			}),
 		password: z
 			.string()
 			.trim()
-			.nonempty({
-				error: UserValidationMessage.PASSWORD_REQUIRED,
-			})
 			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
 				error: UserValidationMessage.PASSWORD_MINIMUM_LENGTH,
 			}),

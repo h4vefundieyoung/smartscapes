f119324c6ce14cf97129ffc51830a510e6a327aa
vldkorin <korinvlad2@gmail.com>
2025-07-29 17:15:01 +0300

    fix(backend): resolve eslint sorting error ss-14

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -2,8 +2,8 @@ const DatabaseTableName = {
 	GROUPS: "groups",
 	GROUPS_TO_PERMISSIONS: "groups_to_permissions",
 	MIGRATIONS: "migrations",
-	POINTS_OF_INTEREST: "points_of_interest",
 	PERMISSIONS: "permissions",
+	POINTS_OF_INTEREST: "points_of_interest",
 	USERS: "users",
 } as const;
 

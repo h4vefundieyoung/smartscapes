63986d6638ff6345008b8bf90b772f05d5ea1588
vldkorin <korinvlad2@gmail.com>
2025-07-30 15:08:26 +0300

    refactor(backend): wrap multiple queries in a single transaction ss-75

--- apps/backend/src/db/migrations/20250727173639_add_reviews_table.ts
+++ apps/backend/src/db/migrations/20250727173639_add_reviews_table.ts
@@ -18,45 +18,47 @@ async function down(knex: Knex): Promise<void> {
 }
 
 async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable(TABLE_NAME, (table) => {
-		table.increments(ColumnName.ID).primary();
-		table
-			.dateTime(ColumnName.CREATED_AT)
-			.notNullable()
-			.defaultTo(knex.fn.now());
-		table
-			.dateTime(ColumnName.UPDATED_AT)
-			.notNullable()
-			.defaultTo(knex.fn.now());
-		table
-			.integer(ColumnName.USER_ID)
-			.notNullable()
-			.references("id")
-			.inTable("users")
-			.onDelete("CASCADE");
-		table.text(ColumnName.CONTENT);
-		table.integer(ColumnName.LIKES_COUNT).defaultTo(0);
-		table
-			.integer(ColumnName.ROUTE_ID)
-			.references("id")
-			.inTable("routes")
-			.onDelete("CASCADE");
-		table
-			.integer(ColumnName.POI_ID)
-			.references("id")
-			.inTable("points_of_interest")
-			.onDelete("CASCADE");
-	});
+	await knex.transaction(async (trx) => {
+		await trx.schema.createTable(TABLE_NAME, (table) => {
+			table.increments(ColumnName.ID).primary();
+			table
+				.dateTime(ColumnName.CREATED_AT)
+				.notNullable()
+				.defaultTo(trx.fn.now());
+			table
+				.dateTime(ColumnName.UPDATED_AT)
+				.notNullable()
+				.defaultTo(trx.fn.now());
+			table
+				.integer(ColumnName.USER_ID)
+				.notNullable()
+				.references("id")
+				.inTable("users")
+				.onDelete("CASCADE");
+			table.text(ColumnName.CONTENT);
+			table.integer(ColumnName.LIKES_COUNT).defaultTo(0);
+			table
+				.integer(ColumnName.ROUTE_ID)
+				.references("id")
+				.inTable("routes")
+				.onDelete("CASCADE");
+			table
+				.integer(ColumnName.POI_ID)
+				.references("id")
+				.inTable("points_of_interest")
+				.onDelete("CASCADE");
+		});
 
-	await knex.raw(`
-		ALTER TABLE "${TABLE_NAME}"
-		ADD CONSTRAINT "check_route_or_poi"
-		CHECK (
-			(${ColumnName.ROUTE_ID} IS NOT NULL AND ${ColumnName.POI_ID} IS NULL)
-			OR
-			(${ColumnName.ROUTE_ID} IS NULL AND ${ColumnName.POI_ID} IS NOT NULL)
+		await trx.raw(`
+			ALTER TABLE "${TABLE_NAME}"
+			ADD CONSTRAINT "check_route_or_poi"
+			CHECK (
+				(${ColumnName.ROUTE_ID} IS NOT NULL AND ${ColumnName.POI_ID} IS NULL)
+				OR
+				(${ColumnName.ROUTE_ID} IS NULL AND ${ColumnName.POI_ID} IS NOT NULL)
 			)
-	`);
+		`);
+	});
 }
 
 export { down, up };

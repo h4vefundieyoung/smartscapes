1f6dbb692d18f54f8bed9f8507b88813c05a6b34
DenisDemirov <124807063+DenisDemirov@users.noreply.github.com>
2025-07-30 21:02:45 +0300

    fix(backend): change error message for user with same email ss-134 (#140)

--- packages/shared/src/modules/users/libs/enums/user-exception-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-exception-message.enum.ts
@@ -1,5 +1,6 @@
 const UserExceptionMessage = {
 	INVALID_CREDENTIALS: "Invalid credentials",
+	USER_ALLREADY_EXISTS: "A user with this email already exists",
 } as const;
 
 export { UserExceptionMessage };

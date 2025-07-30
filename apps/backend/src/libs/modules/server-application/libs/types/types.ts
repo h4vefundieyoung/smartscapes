bc5ff87c7b10b2550eb02aabc4bcad86ccb4155e
Paliantsa Bohdan <49441048+h4vefundieyoung@users.noreply.github.com>
2025-07-30 10:49:38 +0300

    feat(backend/shared): add protected routing ss-23 (#82)
    * feat(backend/shared): implement auth-plugin basement ss-23
    
    * feat(backend): implement auth-plugin user injection ss-23
    
    * feat(backend): inject user entity into controller handler ss-23
    
    * feat(backend/shared): add auth error object to response ss-23
    
    * refactor(backend/shared): refactor auth-plugin module to functional approach ss-23
    
    * refactor(backend): remove jwt payload nesting ss-23
    
    * refactor(backend): rename UserGetAllItemResponseDto interface ss-23
    
    * feat(backend): add error tracing to auth plugin ss-23
    
    * fix(backend): remove reduntant server error ss-23
    
    * feat(backend): handle unexpected errors ss-23
    
    * refactor(backend): move error handling into upper scope ss-23
    
    * refactor(backend): move white routes config to constants ss-23
    
    * refactor(backend): rename UserItemResponseDto interface ss-23
    
    * refactor(backend): remove redundant code & add types ss-23
    
    * feat(backend): update base controller test with injected user prop ss-23
    
    * fix(backend): fix text logic ss-23

--- apps/backend/src/libs/modules/server-application/libs/types/types.ts
+++ apps/backend/src/libs/modules/server-application/libs/types/types.ts
@@ -2,3 +2,4 @@ export { type APIDoc } from "./api-document.type.js";
 export { type ServerApplicationApi } from "./server-application-api.type.js";
 export { type ServerApplicationRouteParameters } from "./server-application-route-parameters.type.js";
 export { type ServerApplication } from "./server-application.type.js";
+export { type WhiteRoute } from "~/libs/modules/plugins/auth/auth.plugin.js";
--- packages/shared/src/modules/auth/libs/enums/enums.ts
+++ packages/shared/src/modules/auth/libs/enums/enums.ts
@@ -1,1 +1,2 @@
 export { AuthApiPath } from "./auth-api-path.enum.js";
+export { AuthExceptionMessage } from "./auth-exception-message.enum.js";

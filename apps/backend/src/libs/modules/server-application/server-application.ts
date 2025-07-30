2831bd8e19b79a749b4350ad0aa5060e65c1f81e
Vladyslav Korin <114109498+vldkorin@users.noreply.github.com>
2025-07-30 18:53:06 +0300

    feat(backend/shared): add reviews module ss-75 (#90)
    * feat(backend/shared): add reviews module ss-75
    
    * refactor(backend): remove user_id foreign key ss-75
    
    * refactor(backend/shared): add constraint, foreign keys, remove validation from service ss-75
    
    * refactor(backend/shared): delete unused files, update validation ss-75
    
    * docs(root): update database schema diagram ss-75
    
    * refactor(backend): update swagger docs ss-75
    
    * fix(backend): solve conflict ss-75
    
    * fix(root/backend): eliminate eslint errors ss-75
    
    * fix(backend): fix eslint sorting error ss-75
    
    * refactor(backend): add FK to reviews table in database schema  ss-75
    
    * refactor(backend): wrap multiple queries in a single transaction ss-75
    
    * refactor(backend):  derive userId from auth context instead of request body ss-75

--- apps/backend/src/libs/modules/server-application/server-application.ts
+++ apps/backend/src/libs/modules/server-application/server-application.ts
@@ -3,6 +3,7 @@ import { database } from "~/libs/modules/database/database.js";
 import { logger } from "~/libs/modules/logger/logger.js";
 import { authController } from "~/modules/auth/auth.js";
 import { pointsOfInterestController } from "~/modules/points-of-interest/points-of-interest.js";
+import { reviewController } from "~/modules/reviews/reviews.js";
 import { routeCategoryController } from "~/modules/route-categories/route-categories.js";
 import { userController } from "~/modules/users/users.js";
 
@@ -13,11 +14,11 @@ const apiV1 = new BaseServerApplicationApi(
 	"v1",
 	config,
 	...authController.routes,
+	...pointsOfInterestController.routes,
+	...reviewController.routes,
 	...routeCategoryController.routes,
 	...userController.routes,
-	...pointsOfInterestController.routes,
 );
-
 const serverApplication = new BaseServerApplication({
 	apis: [apiV1],
 	config,
---
+++ packages/shared/src/modules/reviews/libs/types/review-request-dto.type.ts
@@ -0,0 +1,7 @@
+type ReviewRequestDto = {
+	content: string;
+	poiId: null | number;
+	routeId: null | number;
+};
+
+export { type ReviewRequestDto };

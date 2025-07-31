b626a7e12ee95c462ccfd817e4300313a208dc10
ZakZh <zak.zh@ukr.net>
2025-07-31 22:11:27 +0300

    fix(frontend): fix inline paddings in header ss-168

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -17,7 +17,11 @@ const Header = ({ user }: Properties): JSX.Element => {
 	return (
 		<header className={styles["header"]}>
 			<Link to={AppRoute.ROOT}>
-				<img alt="SmartScapes Logo" height={24} src={appLogo} width={136} />
+				<img
+					alt="SmartScapes Logo"
+					className={styles["header-logo"]}
+					src={appLogo}
+				/>
 			</Link>
 			{hasUser ? (
 				<div className={styles["user-info"]}>

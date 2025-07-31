d3352450e3edd67f4ab73f4ee207cbadf7845b37
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-08-01 01:03:46 +0300

    fix(frontend): fix layout ss-161

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -6,7 +6,8 @@ import {
 	Sidebar,
 } from "~/libs/components/components.js";
 import { NAVIGATION_ITEMS } from "~/libs/constants/constants.js";
-import { useAppDispatch, useEffect } from "~/libs/hooks/hooks.js";
+import { AppRoute } from "~/libs/enums/enums.js";
+import { useAppDispatch, useEffect, useLocation } from "~/libs/hooks/hooks.js";
 import { actions as userActions } from "~/modules/users/users.js";
 
 import { mockImages } from "../carousel/assets/mock-images/mock-images.js";
@@ -14,35 +15,42 @@ import { Carousel } from "../carousel/carousel.js";
 import styles from "./styles.module.css";
 
 const App = (): React.JSX.Element => {
+	const { pathname } = useLocation();
 	const dispatch = useAppDispatch();
 	const mockUserWithoutAvatar = {
 		avatarUrl: null,
 		firstName: "John",
 		lastName: "Smith",
 	};
+
+	const isRoot = pathname === AppRoute.APP;
+
 	useEffect(() => {
-		void dispatch(userActions.loadAll());
-	}, [dispatch]);
+		if (isRoot) {
+			void dispatch(userActions.loadAll());
+		}
+	}, [isRoot, dispatch]);
 
 	return (
 		<div className={styles["container"]}>
 			<div className={styles["outlet-container"]}>
 				<RouterOutlet />
 			</div>
-
-			<div className={styles["components-container"]}>
-				<Header user={mockUserWithoutAvatar} />
-				<div className={styles["sidebar-container"]}>
-					<Sidebar navigationItems={NAVIGATION_ITEMS} />
+			{isRoot && (
+				<div className={styles["components-container"]}>
+					<Header user={mockUserWithoutAvatar} />
+					<div className={styles["sidebar-container"]}>
+						<Sidebar navigationItems={NAVIGATION_ITEMS} />
+					</div>
+					<Loader />
+					<div className={styles["button-container"]}>
+						<Button label="Button for test" type="button" />
+					</div>
+					<div className={styles["carousel-container"]}>
+						<Carousel images={mockImages} />
+					</div>
 				</div>
-				<Loader />
-				<div className={styles["button-container"]}>
-					<Button label="Button for test" type="button" />
-				</div>
-				<div className={styles["carousel-container"]}>
-					<Carousel images={mockImages} />
-				</div>
-			</div>
+			)}
 		</div>
 	);
 };

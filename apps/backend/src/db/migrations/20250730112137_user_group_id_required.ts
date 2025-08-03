507b0c79d146739377aaa8790e6c2af089c93b8c
DenisDemirov <demirov_denis@ukr.net>
2025-08-03 16:17:38 +0300

    fix(backend): add group_id selection from query by key value ss-113

--- apps/backend/src/db/migrations/20250730112137_user_group_id_required.ts
+++ apps/backend/src/db/migrations/20250730112137_user_group_id_required.ts
@@ -1,24 +1,36 @@
 import { type Knex } from "knex";
 
-const TABLE_NAME = "users";
+const TABLE_NAME = {
+	GROUP: "groups",
+	USER: "users",
+} as const;
 
 const ColumnName = {
 	GROUP_ID: "group_id",
+	ID: "id",
+	KEY: "key",
 } as const;
 
+const SINGLE = 1;
+
 async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable(TABLE_NAME, (table) => {
+	await knex.schema.alterTable(TABLE_NAME.USER, (table) => {
 		table.integer(ColumnName.GROUP_ID).nullable().alter();
 	});
 }
 
 async function up(knex: Knex): Promise<void> {
 	await knex.transaction(async (trx) => {
-		await trx(TABLE_NAME)
+		await trx(TABLE_NAME.USER)
 			.whereNull(ColumnName.GROUP_ID)
-			.update({ [ColumnName.GROUP_ID]: 2 });
+			.update({
+				[ColumnName.GROUP_ID]: knex(TABLE_NAME.GROUP)
+					.select(ColumnName.ID)
+					.where(ColumnName.KEY, "users")
+					.limit(SINGLE),
+			});
 
-		await trx.schema.alterTable(TABLE_NAME, (table) => {
+		await trx.schema.alterTable(TABLE_NAME.USER, (table) => {
 			table.integer(ColumnName.GROUP_ID).notNullable().alter();
 		});
 	});

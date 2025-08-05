3c3650f2e853dbff792b93108f295fd95f209be1
DenisDemirov <demirov_denis@ukr.net>
2025-08-05 09:48:37 +0300

    fix(shared): remove unnecessary group field inUserSignInResponseDto ss-113

--- packages/shared/src/modules/users/libs/types/user-sign-in-response-dto.type.ts
+++ packages/shared/src/modules/users/libs/types/user-sign-in-response-dto.type.ts
@@ -1,8 +1,6 @@
-import { type GroupAuthResponseDto } from "../../../groups/types/types.js";
 import { type UserAuthResponseDto } from "./user-auth-response-dto.type.js";
 
 type UserSignInResponseDto = {
-	group: GroupAuthResponseDto;
 	token: string;
 	user: Pick<UserAuthResponseDto, "email" | "id">;
 };

36c24003a75ae201967025a4303b671173028705
whispermind <h4vefun.dieyoung@gmail.com>
2025-08-05 21:43:03 +0300

    feat(backend): add swagger docs ss-193

--- apps/backend/src/modules/routes/routes.controller.ts
+++ apps/backend/src/modules/routes/routes.controller.ts
@@ -20,6 +20,92 @@ const SECONDS = 60;
 const SECOND_MS = 1000;
 const MINUTE_MS = SECONDS * SECOND_MS;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Coordinate:
+ *       type: array
+ *       items:
+ *         type: number
+ *       minItems: 2
+ *       maxItems: 2
+ *       example: [30.5, 50.4]
+ *
+ *     GetMapBoxRouteResponseDto:
+ *       type: object
+ *       properties:
+ *         code:
+ *           type: string
+ *         routes:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/MapBoxRoute'
+ *         uuid:
+ *           type: string
+ *         waypoints:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/MapBoxWaypoint'
+ *
+ *     MapBoxRoute:
+ *       type: object
+ *       properties:
+ *         distance:
+ *           type: number
+ *         duration:
+ *           type: number
+ *         geometry:
+ *           type: object
+ *           properties:
+ *             coordinates:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Coordinate'
+ *             type:
+ *               type: string
+ *         legs:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/MapBoxLegs'
+ *         weight:
+ *           type: number
+ *         weight_name:
+ *           type: string
+ *
+ *     MapBoxLegs:
+ *       type: object
+ *       properties:
+ *         admins:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               iso_3166_1:
+ *                 type: string
+ *               iso_3166_1_alpha3:
+ *                 type: string
+ *         distance:
+ *           type: number
+ *         duration:
+ *           type: number
+ *         summary:
+ *           type: string
+ *         weight:
+ *           type: number
+ *
+ *     MapBoxWaypoint:
+ *       type: object
+ *       properties:
+ *         distance:
+ *           type: number
+ *         location:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Coordinate'
+ *         name:
+ *           type: string
+ */
 class RoutesController extends BaseController {
 	private debounceMap: Record<number, number>;
 	private routesService: RoutesService;
@@ -40,6 +126,40 @@ class RoutesController extends BaseController {
 		});
 	}
 
+	/**
+	 * @swagger
+	 * /routes/construct:
+	 *   post:
+	 *     security:
+	 *       - bearerAuth: []
+	 *     tags:
+	 *       - Routes
+	 *     summary: Construct Mapbox route
+	 *     requestBody:
+	 *       required: true
+	 *       content:
+	 *         application/json:
+	 *           schema:
+	 *             type: object
+	 *             required:
+	 *               - pointsOfInterest
+	 *             properties:
+	 *               pointsOfInterest:
+	 *                 type: array
+	 *                 items:
+	 *                   type: integer
+	 *                 example: [1,2]
+	 *     responses:
+	 *       200:
+	 *         description: Mapbox service response
+	 *         content:
+	 *           application/json:
+	 *             schema:
+	 *               type: object
+	 *               properties:
+	 *                 data:
+	 *                   $ref: '#/components/schemas/GetMapBoxRouteResponseDto'
+	 */
 	private async constructRoute({
 		body: { pointsOfInterest },
 		user,

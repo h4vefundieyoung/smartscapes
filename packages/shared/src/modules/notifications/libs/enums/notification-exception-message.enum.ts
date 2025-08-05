e2b4a1514dd0a10f89a36d8bc3894c61444f168f
my-derkach <my.derkach@gmail.com>
2025-08-05 20:50:36 +0200

    feat(shared/backend): add notifications module ss-73 (#131)
    * feat(backend): add new table notifications to database using migrations ss-75
    
    * feat(shared): add notification api path ss-75
    
    * feat(shared): define notification and entity types for notification module ss-75
    
    * feat(shared): define notification exception message ss-75
    
    * feat(shared): add notification get all item response dto ss-75
    
    * feat(shared): add notification create request dto ss-75
    
    * feat(shared): add notification validation rule ss-75
    
    * feat(shared): add notification validation message ss-75
    
    * feat(shared): add notification create validation schema ss-75
    
    * feat(backend): implement notification model, entity, repository, service and controller ss-73
    
    * fix(backend): fix mistype issue with notification service ss-73
    
    * feat(backend): create tests for notification entity ss-73
    
    * feat(backend): create tests for notification repository ss-73
    
    * feat(backend): create tests for notification service ss-73
    
    * feat(backend): add tests for notification controller ss-73
    
    * fix(backend): resolve merge conflict and update notification controller ss-73
    
    * feat(backend): add swagger documentation to notfication controller methods ss-73
    
    * feat(root): create mermaid diagram for notification table ss-73
    
    * fix(backend): add required missing properties for testing ss-73
    
    * feat(shared/backend): create custom exception class for notifications ss-73
    
    * feat(backend): define notification database column ss-73
    
    * feat(backend): update notifications migrations with trancsaction ss-73
    
    * fix(shared/backend): rename entity type to notification entity type ss-73
    
    * fix(backend): remove unsigned from user is since it is unnecessary ss-73

---
+++ packages/shared/src/modules/notifications/libs/enums/notification-exception-message.enum.ts
@@ -0,0 +1,7 @@
+const NotificationExceptionMessage = {
+	NOTIFICATION_CREATE_FAIL: "Failed to create notification",
+	NOTIFICATION_NOT_FOUND: "Notification not found.",
+	NOTIFICATION_UNAUTHORIZED: "Unauthorized access",
+} as const;
+
+export { NotificationExceptionMessage };

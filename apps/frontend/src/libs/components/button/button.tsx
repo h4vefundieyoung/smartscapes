69205e0af72093998e9a8dae3de3c6ecbd603b5d
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-08-07 01:08:15 +0300

    revert(frontend): buttom component props ss-185

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,9 +1,6 @@
-import { combineClassNames } from "~/libs/helpers/combine-class-names.helper.js";
-
 import styles from "./styles.module.css";
 
 type Properties = {
-	className?: string | undefined;
 	label: string;
 	onClick?: () => void;
 	to?: string;
@@ -11,24 +8,21 @@ type Properties = {
 };
 
 const Button = ({
-	className,
 	label,
 	onClick,
 	to,
 	type = "submit",
 }: Properties): React.JSX.Element => {
-	const buttonClassName = combineClassNames(styles["button"], className);
-
 	if (to) {
 		return (
-			<a className={buttonClassName} href={to}>
+			<a className={styles["button"]} href={to}>
 				{label}
 			</a>
 		);
 	}
 
 	return (
-		<button className={buttonClassName} onClick={onClick} type={type}>
+		<button className={styles["button"]} onClick={onClick} type={type}>
 			{label}
 		</button>
 	);

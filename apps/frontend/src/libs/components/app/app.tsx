18675fe98765da44327909ca05e80d138d074640
timosh-yura <tron15963@gmail.com>
2025-08-06 16:22:22 +0300

    revert(frontend): revert token check ss-24

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,7 +1,6 @@
 import { RouterOutlet } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { useAppDispatch, useEffect, useLocation } from "~/libs/hooks/hooks.js";
-import { storage, StorageKey } from "~/libs/modules/storage/storage.js";
 import { actions as authActions } from "~/modules/auth/auth.js";
 import { actions as userActions } from "~/modules/users/users.js";
 
@@ -20,15 +19,7 @@ const App = (): React.JSX.Element => {
 	}, [isRoot, dispatch]);
 
 	useEffect(() => {
-		const fetchToken = async (): Promise<void> => {
-			const hasToken = await storage.get(StorageKey.TOKEN);
-
-			if (hasToken) {
-				void dispatch(authActions.getAuthenticatedUser());
-			}
-		};
-
-		void fetchToken();
+		void dispatch(authActions.getAuthenticatedUser());
 	}, [dispatch]);
 
 	return (

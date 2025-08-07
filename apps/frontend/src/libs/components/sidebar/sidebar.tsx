1d73c4a02420d5631e17b8589a2597c3c31f5967
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-07 20:17:29 +0200

    feat(frontend): add floating sidebar ss-106 (#214)
    * feat(frontend): make sidebar collapsible ss-106
    
    * feat(frontend): remove sidebar container ss-106
    
    * refactor(frontend): change dashboard href to /app ss-106
    
    * fix(frontend): fix sidebar item movement on sidebar opening ss-106
    
    * feat(frontend): remade sidebar ss-106
    
    * refactor(frontend): remove unnecessary conditional rendering ss-106
    
    * feat(frontend): add useState hook export ss-106
    
    * feat(frontend): add KeyboardKey enum ss-106
    
    * feat(frontend): add container query ss-106
    
    * refactor(frontend): update styles, change backdrop to div, add visually-hidden to logo ss-106
    
    * chore(frontend): update version ss-106
    
    * feat(frontend): add nav icons ss-106
    
    * feat(frontend): add routes ss-106
    
    * feat(frontend): add amin navigation ss-106
    
    * feat(frontend): add navigation items groups ss-106
    
    * feat(frontend): add isLabelHidden toggle ss-106
    
    * chore(frontend): move keyboard key enum to libs/enums ss-106
    
    * feat(frontend): update navigation items rendering logic ss-106
    
    * fix(frontend): fix container width, remove redundant rule ss-106
    
    * refactor(frontend): update className assignment condition ss-106

--- apps/frontend/src/libs/components/sidebar/sidebar.tsx
+++ apps/frontend/src/libs/components/sidebar/sidebar.tsx
@@ -1,37 +1,107 @@
 import smartScapesLogo from "~/assets/images/logo.svg";
 import { Link } from "~/libs/components/components.js";
-import { AppRoute } from "~/libs/enums/enums.js";
-import { type NavigationItem } from "~/libs/types/types.js";
+import { AppRoute, KeyboardKey } from "~/libs/enums/enums.js";
+import { combineClassNames } from "~/libs/helpers/helpers.js";
+import { useCallback, useEffect, useState } from "~/libs/hooks/hooks.js";
+import { type NavigationItemsGroup } from "~/libs/types/types.js";
 
 import { SidebarItem } from "./libs/components/components.js";
 import styles from "./styles.module.css";
 
 type Properties = {
-	navigationItems: NavigationItem[];
+	navigationItemsGroups: NavigationItemsGroup[];
 };
 
-const Sidebar = ({ navigationItems }: Properties): React.JSX.Element => {
+const Sidebar = ({ navigationItemsGroups }: Properties): React.JSX.Element => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const handleClick = useCallback((): void => {
+		setIsOpen((previous) => !previous);
+	}, [setIsOpen]);
+
+	const handleKeyDown = useCallback(
+		(event: KeyboardEvent | React.KeyboardEvent) => {
+			if (event.key === KeyboardKey.ESCAPE) {
+				setIsOpen(false);
+			}
+		},
+		[setIsOpen],
+	);
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown);
+
+		return (): void => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [setIsOpen, handleKeyDown]);
+
 	return (
-		<div className={styles["sidebar"]}>
-			<Link to={AppRoute.ROOT}>
-				<img
-					alt="SmartScapes"
-					className={styles["logo"]}
-					height="24"
-					src={smartScapesLogo}
-					width="136"
+		<div className={styles["container"]}>
+			<div className={styles["drawer"]} data-open={isOpen}>
+				<div className={styles["header"]}>
+					<button
+						className={styles["close-button"]}
+						data-open={isOpen}
+						onClick={handleClick}
+						type="button"
+					>
+						<span className={styles["burger-icon"]} />
+					</button>
+
+					<div className={isOpen ? "" : "visually-hidden"}>
+						<Link to={AppRoute.ROOT}>
+							<img
+								alt="SmartScapes"
+								className={styles["logo-image"]}
+								height="24"
+								src={smartScapesLogo}
+								width="136"
+							/>
+						</Link>
+					</div>
+				</div>
+				<div className={styles["sidebar"]}>
+					{navigationItemsGroups.map(({ hasLabel, items, name }) => (
+						<div
+							className={combineClassNames(
+								styles["group"],
+								hasLabel && styles["shifted"],
+							)}
+							key={name}
+						>
+							{hasLabel && (
+								<div
+									className={combineClassNames(
+										styles["title"],
+										!isOpen && "visually-hidden",
+									)}
+								>
+									<span className={styles["title-text"]}>{name}</span>
+								</div>
+							)}
+							<ul className={styles["navigation-list"]}>
+								{items.map((item) => (
+									<SidebarItem
+										href={item.href}
+										icon={item.icon}
+										isLabelHidden={!isOpen}
+										key={item.icon}
+										label={item.label}
+									/>
+								))}
+							</ul>
+						</div>
+					))}
+				</div>
+			</div>
+			{isOpen && (
+				<div
+					aria-hidden="true"
+					className={styles["backdrop"]}
+					onClick={handleClick}
 				/>
-			</Link>
-			<ul className={styles["navigation-list"]}>
-				{navigationItems.map((item) => (
-					<SidebarItem
-						href={item.href}
-						icon={item.icon}
-						key={item.icon}
-						label={item.label}
-					/>
-				))}
-			</ul>
+			)}
 		</div>
 	);
 };

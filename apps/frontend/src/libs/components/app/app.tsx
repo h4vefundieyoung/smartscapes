392d77985c2b3d44b5a68a8ae0da7f65f54fb168
timosh-yura <tron15963@gmail.com>
2025-08-07 12:31:18 +0300

    fix(frontend): app component waits for get-authicated-user dispatch ss-247

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,6 +1,11 @@
-import { RouterOutlet } from "~/libs/components/components.js";
+import { Loader, RouterOutlet } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { useAppDispatch, useEffect, useLocation } from "~/libs/hooks/hooks.js";
+import {
+	useAppDispatch,
+	useEffect,
+	useLocation,
+	useState,
+} from "~/libs/hooks/hooks.js";
 import { actions as authActions } from "~/modules/auth/auth.js";
 import { actions as userActions } from "~/modules/users/users.js";
 
@@ -9,6 +14,7 @@ import styles from "./styles.module.css";
 const App = (): React.JSX.Element => {
 	const { pathname } = useLocation();
 	const dispatch = useAppDispatch();
+	const [isAuthInitialized, setIsAuthInitialized] = useState<boolean>(false);
 
 	const isRoot = pathname === AppRoute.APP;
 
@@ -19,8 +25,21 @@ const App = (): React.JSX.Element => {
 	}, [isRoot, dispatch]);
 
 	useEffect(() => {
-		void dispatch(authActions.getAuthenticatedUser());
+		const initAuth = async (): Promise<void> => {
+			await dispatch(authActions.getAuthenticatedUser());
+			setIsAuthInitialized(true);
+		};
+
+		void initAuth();
 	}, [dispatch]);
+
+	if (!isAuthInitialized) {
+		return (
+			<div className={styles["loader-container"]}>
+				<Loader />
+			</div>
+		);
+	}
 
 	return (
 		<div className={styles["container"]}>

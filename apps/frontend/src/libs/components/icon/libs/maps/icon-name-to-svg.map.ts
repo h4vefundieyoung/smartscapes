7a4434572ff358364e0a40198ab2dcc6df64af9c
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-07 13:12:38 +0200

    feat(frontend): add nav icons ss-106

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -1,5 +1,10 @@
 import Analytics from "~/assets/images/icons/analytics.svg?react";
 import Dashboard from "~/assets/images/icons/dashboard.svg?react";
+import Map from "~/assets/images/icons/map.svg?react";
+import Message from "~/assets/images/icons/message.svg?react";
+import Route from "~/assets/images/icons/route.svg?react";
+import Tag from "~/assets/images/icons/tag.svg?react";
+import User from "~/assets/images/icons/user.svg?react";
 import { type IconName } from "~/libs/types/types.js";
 
 const iconNameToSvg: Record<
@@ -8,6 +13,11 @@ const iconNameToSvg: Record<
 > = {
 	analytics: Analytics,
 	dashboard: Dashboard,
+	map: Map,
+	message: Message,
+	route: Route,
+	tag: Tag,
+	user: User,
 };
 
 export { iconNameToSvg };

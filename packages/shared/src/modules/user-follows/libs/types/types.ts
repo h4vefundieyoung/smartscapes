44cf9baa78847c7b9c9aa2720218149ea1b759e5
Dima Bohachuk <dima.b.02@ukr.net>
2025-08-09 11:18:28 +0300

    refactor(shared): refactored all naming problems ss-72

--- packages/shared/src/modules/user-follows/libs/types/types.ts
+++ packages/shared/src/modules/user-follows/libs/types/types.ts
@@ -1,3 +1,3 @@
-export { type UserFollowsFollowParametersDto } from "./user-follows-follow-parameters-dto.types.js";
-export { type UserFollowsFollowRequestDto } from "./user-follows-follow-request-dto.types.js";
-export { type UserFollowsUnfollowParametersDto } from "./user-follows-unfollow-parameters-dto.types.js";
+export { type UserFollowsFollowParametersDto } from "./user-follows-follow-parameters-dto.type.js";
+export { type UserFollowsFollowRequestDto } from "./user-follows-follow-request-dto.type.js";
+export { type UserFollowsUnfollowParametersDto } from "./user-follows-unfollow-parameters-dto.type.js";

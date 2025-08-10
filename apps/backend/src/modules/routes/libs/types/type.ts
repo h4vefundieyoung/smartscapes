a9f07cf96dfc22d8dbddbeba7fa3abed0ec22c5d
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-10 17:44:59 +0200

    feat(backend): add RoutesFindAllOptions type ss-267

--- apps/backend/src/modules/routes/libs/types/type.ts
+++ apps/backend/src/modules/routes/libs/types/type.ts
@@ -1,5 +1,7 @@
+export { type RoutesFindAllOptions } from "./routes-find-all-options.type.js";
 export {
 	type RoutesRequestCreateDto,
 	type RoutesRequestPatchDto,
 	type RoutesResponseDto,
+	type RoutesSearchQuery,
 } from "@smartscapes/shared";

e1cd22d93b21fccad71568395e4bdc788fff189f
vldkorin <korinvlad2@gmail.com>
2025-08-10 19:23:57 +0300

    feat(frontend): add textarea component ss-244

---
+++ apps/frontend/src/libs/components/textarea/textarea.tsx
@@ -0,0 +1,48 @@
+import {
+	type Control,
+	type FieldErrors,
+	type FieldPath,
+	type FieldValues,
+} from "react-hook-form";
+
+import { useFormController } from "~/libs/hooks/hooks.js";
+
+import styles from "./styles.module.css";
+
+type Properties<TFieldValues extends FieldValues> = {
+	control: Control<TFieldValues, null>;
+	errors: FieldErrors<TFieldValues>;
+	label: string;
+	name: FieldPath<TFieldValues>;
+	placeholder?: string;
+};
+
+const TextArea = <TFieldValues extends FieldValues>({
+	control,
+	errors,
+	label,
+	name,
+	placeholder,
+}: Properties<TFieldValues>): React.JSX.Element => {
+	const { field } = useFormController({ control, name });
+
+	const fieldError = (
+		errors as unknown as Record<string, undefined | { message?: string }>
+	)[name];
+
+	return (
+		<label className={styles["label"]}>
+			<span className={styles["caption"]}>{label}</span>
+			<textarea
+				{...field}
+				className={styles["textarea"]}
+				placeholder={placeholder}
+			/>
+			{fieldError?.message && (
+				<span className={styles["error"]}>{String(fieldError.message)}</span>
+			)}
+		</label>
+	);
+};
+
+export { TextArea };

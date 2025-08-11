ce9a7adbc1e6caa122114f8d299bdb256f731f43
DenisDemirov <demirov_denis@ukr.net>
2025-08-11 11:46:32 +0300

    Revert "fix(backend): find password detail added return null ss-113"
    This reverts commit 0bf0eeddd721abb55d4feb99155b14660ecd1ee1.

--- apps/backend/src/modules/users/user.repository.ts
+++ apps/backend/src/modules/users/user.repository.ts
@@ -174,7 +174,7 @@ class UserRepository implements Repository {
 	public async findPasswordDetails(
 		email: string,
 	): Promise<null | UserPasswordDetails> {
-		const user = await this.userModel
+		const user = (await this.userModel
 			.query()
 			.select(
 				"users.id",
@@ -186,11 +186,7 @@ class UserRepository implements Repository {
 			)
 			.withGraphJoined("group.permissions")
 			.where("users.email", email)
-			.first();
-
-		if (!user) {
-			return null;
-		}
+			.first()) as UserModel;
 
 		const group = user.group as NonNullable<typeof user.group>;
 		const permissions = user.group?.permissions as NonNullable<

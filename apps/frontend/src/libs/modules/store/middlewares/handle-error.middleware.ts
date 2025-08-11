27923905cb43279bb96d338afb8ddd477be4d0e0
whispermind <h4vefun.dieyoung@gmail.com>
2025-08-11 03:27:37 +0300

    fix(frontend): allows 401 notifications from sign-in action ss-184

--- apps/frontend/src/libs/modules/store/middlewares/handle-error.middleware.ts
+++ apps/frontend/src/libs/modules/store/middlewares/handle-error.middleware.ts
@@ -3,12 +3,16 @@ import { isRejected, type Middleware } from "@reduxjs/toolkit";
 import { CommonExceptionMessage } from "~/libs/enums/enums.js";
 import { AUTH_ERROR_DEFAULT_NAME } from "~/libs/modules/http/http.js";
 import { type ExtraArguments } from "~/libs/modules/store/libs/types/types.js";
+import { signIn } from "~/modules/auth/slices/actions.js";
 
 const handleError = ({ toastNotifier }: ExtraArguments): Middleware => {
 	return () => {
 		return (next) => (action) => {
 			if (isRejected(action)) {
-				if (action.error.name === AUTH_ERROR_DEFAULT_NAME) {
+				if (
+					action.error.name === AUTH_ERROR_DEFAULT_NAME &&
+					action.type !== signIn.rejected.type
+				) {
 					return;
 				}
 

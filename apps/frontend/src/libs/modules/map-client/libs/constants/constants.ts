ff60cc510a380b999b857d27b609f91d70afc16c
Zakhar Zhelizniak <38009936+ZakZh@users.noreply.github.com>
2025-08-11 10:47:18 +0300

    feat(frontend): implement basic maps integration ss-116 (#215)
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement-basic-maps-integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    * feat(frontend): cleanup map module and delete unused ss-116
    
    * feat(frontend): change map module methods ss-116
    
    * feat(frontend): impelement basic maps integration ss-116
    
    * feat(frontend): refactor map types and delete some marker control logic ss-116
    
    * feat(frontend): implement basic maps integration ss-116
    
    ---------
    
    Co-authored-by: Zakhar Zheliznyak <C:\Users\zakzh\AppData\Roaming\The Bat!>

---
+++ apps/frontend/src/libs/modules/map-client/libs/constants/constants.ts
@@ -0,0 +1,8 @@
+export { GEOLOCATE_AUTO_TRIGGER_DELAY } from "./geolocate-auto-trigger-delay.constant.js";
+export { GEOLOCATE_CONTROL_OPTIONS } from "./geolocate-control-options.constant.js";
+export { MAP_CONTROLS_POSITION } from "./map-controls-position.constant.js";
+export { MAP_OPTIONS } from "./map-options.constant.js";
+export { MARKER_OPTIONS } from "./marker-options.constant.js";
+export { NAVIGATION_CONTROL_OPTIONS } from "./navigation-control-options.constant.js";
+export { SCALE_CONTROL_OPTIONS } from "./scale-control-options.constant.js";
+export { ZOOM_LEVEL } from "./zoom-level.constant.js";

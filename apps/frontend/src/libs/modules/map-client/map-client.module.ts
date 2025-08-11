53cc0c4a3de7f19a35dd524d6e44d262f6f4cda6
Yelyzaveta Veis <liza.veis14@gmail.com>
2025-08-11 18:34:12 +0300

    fix(frontend): set headers for mapbox api ss-295

--- apps/frontend/src/libs/modules/map-client/map-client.module.ts
+++ apps/frontend/src/libs/modules/map-client/map-client.module.ts
@@ -1,3 +1,5 @@
+import mapboxgl from "mapbox-gl";
+
 import { config } from "~/libs/modules/config/config.js";
 
 import {
@@ -11,13 +13,13 @@ import {
 	ZOOM_LEVEL,
 } from "./libs/constants/constants.js";
 import { MapControlId } from "./libs/enums/enums.js";
-import { mapboxgl } from "./libs/modules/mapbox/mapbox.js";
 import {
 	type ControlPosition,
 	type MapControl,
 	type MapOptions,
 	type PointGeometry,
 } from "./libs/types/types.js";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 class MapClient {
 	private accessToken: MapOptions["accessToken"];

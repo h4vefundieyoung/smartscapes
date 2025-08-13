b0c1b44ae59c7145ae939874921532a747a036b3
DenisDemirov <demirov_denis@ukr.net>
2025-08-13 14:02:42 +0300

    feat(backend): add migration for create table categories and update table route categories ss-345

---
+++ apps/backend/src/db/migrations/20250813100759_update_route_categories_and_add_categories_table.ts
@@ -0,0 +1,67 @@
+import { type Knex } from "knex";
+
+const TableName = {
+	CATEGORIES: "categories",
+	ROUTE_CATEGORIES: "route_categories",
+	ROUTES: "routes",
+};
+
+const ColumnName = {
+	CATEGORY_ID: "category_id",
+	CREATED_AT: "created_at",
+	ID: "id",
+	KEY: "key",
+	NAME: "name",
+	ROUTE_ID: "route_id",
+	UPDATED_AT: "updated_at",
+};
+
+async function down(knex: Knex): Promise<void> {
+	await knex.transaction(async (trx) => {
+		await trx.schema.alterTable(TableName.ROUTE_CATEGORIES, (table) => {
+			table.dropColumn(ColumnName.CATEGORY_ID);
+			table.dropColumn(ColumnName.ROUTE_ID);
+			table.string(ColumnName.NAME).unique().notNullable();
+		});
+
+		await trx.schema.dropTableIfExists(TableName.CATEGORIES);
+	});
+}
+
+async function up(knex: Knex): Promise<void> {
+	await knex.transaction(async (trx) => {
+		await trx.schema.createTable(TableName.CATEGORIES, (table) => {
+			table.increments(ColumnName.ID).primary();
+			table
+				.timestamp(ColumnName.CREATED_AT)
+				.notNullable()
+				.defaultTo(trx.fn.now());
+			table
+				.timestamp(ColumnName.UPDATED_AT)
+				.notNullable()
+				.defaultTo(trx.fn.now());
+			table.string(ColumnName.KEY).unique().notNullable();
+			table.string(ColumnName.NAME).notNullable();
+		});
+
+		await trx.schema.alterTable(TableName.ROUTE_CATEGORIES, (table) => {
+			table.dropColumn(ColumnName.NAME);
+
+			table
+				.integer(ColumnName.CATEGORY_ID)
+				.notNullable()
+				.references(ColumnName.ID)
+				.inTable(TableName.CATEGORIES)
+				.onDelete("CASCADE");
+
+			table
+				.integer(ColumnName.ROUTE_ID)
+				.notNullable()
+				.references(ColumnName.ID)
+				.inTable(TableName.ROUTES)
+				.onDelete("CASCADE");
+		});
+	});
+}
+
+export { down, up };

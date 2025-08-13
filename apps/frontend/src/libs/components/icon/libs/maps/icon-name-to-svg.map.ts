442e98579c45b77530bf0aa33b6c167218ac1544
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-14 00:22:36 +0200

    feat(frontend): add new icons ss-340

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -1,10 +1,12 @@
 import Analytics from "~/assets/images/icons/analytics.svg?react";
 import ArrowDown from "~/assets/images/icons/arrow-down.svg?react";
 import Dashboard from "~/assets/images/icons/dashboard.svg?react";
+import Link from "~/assets/images/icons/link.svg?react";
 import Map from "~/assets/images/icons/map.svg?react";
 import Message from "~/assets/images/icons/message.svg?react";
 import Route from "~/assets/images/icons/route.svg?react";
 import Tag from "~/assets/images/icons/tag.svg?react";
+import Trash from "~/assets/images/icons/trash.svg?react";
 import User from "~/assets/images/icons/user.svg?react";
 import { type IconName } from "~/libs/types/types.js";
 
@@ -15,10 +17,12 @@ const iconNameToSvg: Record<
 	analytics: Analytics,
 	arrowDown: ArrowDown,
 	dashboard: Dashboard,
+	link: Link,
 	map: Map,
 	message: Message,
 	route: Route,
 	tag: Tag,
+	trash: Trash,
 	user: User,
 };
 
--- apps/frontend/src/libs/types/icon-name.type.ts
+++ apps/frontend/src/libs/types/icon-name.type.ts
@@ -2,10 +2,12 @@ type IconName =
 	| "analytics"
 	| "arrowDown"
 	| "dashboard"
+	| "link"
 	| "map"
 	| "message"
 	| "route"
 	| "tag"
+	| "trash"
 	| "user";
 
 export { type IconName };

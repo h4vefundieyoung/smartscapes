b7dfdf3c6c74c484b18c31a2a6d508d31975852f
vldkorin <korinvlad2@gmail.com>
2025-08-14 23:28:40 +0300

    refactor(frontend): update map-client module with methods to set markers ss-359

--- apps/frontend/src/libs/modules/map-client/map-client.module.ts
+++ apps/frontend/src/libs/modules/map-client/map-client.module.ts
@@ -24,11 +24,22 @@ import "mapbox-gl/dist/mapbox-gl.css";
 class MapClient {
 	private accessToken: MapOptions["accessToken"];
 	private controls = new Map<string, MapControl>();
+	private isClickAttached = false;
 	private map: mapboxgl.Map | null = null;
+
+	private onMapClickBound: (event: mapboxgl.MapMouseEvent) => void;
+	private onMapLoadAttachBound: () => void;
+
+	private onSelectCallback: ((coords: [number, number]) => void) | null = null;
+	private readyCallbacks: Array<() => void> = [];
+
 	private resizeObserver: null | ResizeObserver = null;
+	private selectedMarker: mapboxgl.Marker | null = null;
 
 	public constructor() {
 		this.accessToken = config.ENV.MAPBOX.ACCESS_TOKEN;
+		this.onMapClickBound = this.onMapClick.bind(this);
+		this.onMapLoadAttachBound = this.onMapLoadAttach.bind(this);
 	}
 
 	public addGeolocateControl(): void {
@@ -87,6 +98,11 @@ class MapClient {
 		);
 	}
 
+	public clearSelected(): void {
+		this.selectedMarker?.remove();
+		this.selectedMarker = null;
+	}
+
 	public destroy(): void {
 		for (const control of this.controls.values()) {
 			this.map?.removeControl(control);
@@ -94,11 +110,38 @@ class MapClient {
 
 		this.controls.clear();
 
+		this.detachClickHandler();
+		this.onSelectCallback = null;
+		this.isClickAttached = false;
+
 		this.resizeObserver?.disconnect();
 		this.resizeObserver = null;
 
 		this.map?.remove();
 		this.map = null;
+		this.selectedMarker = null;
+		this.readyCallbacks = [];
+	}
+
+	public enableClickToSelect(
+		onSelect: (coords: [number, number]) => void,
+	): () => void {
+		this.onSelectCallback = onSelect;
+
+		if (this.map) {
+			return this.registerClickHandler();
+		}
+
+		let innerCleanup: () => void = this.noop;
+		const unsub = this.onReady(() => {
+			innerCleanup = this.registerClickHandler();
+		});
+
+		return () => {
+			unsub();
+			innerCleanup();
+			this.onSelectCallback = null;
+		};
 	}
 
 	public flyTo(center: [number, number]): void {
@@ -107,12 +150,7 @@ class MapClient {
 		}
 
 		this.map.stop();
-
-		this.map.flyTo({
-			center,
-			essential: true,
-			zoom: ZOOM_LEVEL,
-		});
+		this.map.flyTo({ center, essential: true, zoom: ZOOM_LEVEL });
 	}
 
 	public init(container: HTMLElement): void {
@@ -130,6 +168,27 @@ class MapClient {
 			this.map?.resize();
 		});
 		this.resizeObserver.observe(container);
+
+		const callbacks = [...this.readyCallbacks];
+		this.readyCallbacks = [];
+
+		for (const callback of callbacks) {
+			callback();
+		}
+	}
+
+	public onReady(callback: () => void): () => void {
+		if (this.map) {
+			callback();
+
+			return this.noop;
+		}
+
+		this.readyCallbacks.push(callback);
+
+		return () => {
+			this.readyCallbacks = this.readyCallbacks.filter((f) => f !== callback);
+		};
 	}
 
 	public resize(): void {
@@ -138,6 +197,20 @@ class MapClient {
 
 	public setCenter(center: [number, number]): void {
 		this.map?.setCenter(center);
+	}
+
+	public setSelected(coordinates: [number, number]): void {
+		if (!this.map) {
+			return;
+		}
+
+		if (this.selectedMarker) {
+			this.selectedMarker.setLngLat(coordinates);
+		} else {
+			this.selectedMarker = new mapboxgl.Marker(MARKER_OPTIONS)
+				.setLngLat(coordinates)
+				.addTo(this.map);
+		}
 	}
 
 	private addControl(
@@ -160,6 +233,61 @@ class MapClient {
 		}
 
 		new mapboxgl.Marker(MARKER_OPTIONS).setLngLat(coordinates).addTo(this.map);
+	}
+
+	private attachClickHandler(): void {
+		if (!this.map || this.isClickAttached) {
+			return;
+		}
+
+		this.map.on("click", this.onMapClickBound);
+		this.isClickAttached = true;
+	}
+
+	private detachClickHandler(): void {
+		if (!this.map || !this.isClickAttached) {
+			return;
+		}
+
+		this.map.off("click", this.onMapClickBound);
+		this.isClickAttached = false;
+	}
+
+	private noop = (): void => {};
+
+	private onMapClick(event: mapboxgl.MapMouseEvent): void {
+		const coords: [number, number] = [event.lngLat.lng, event.lngLat.lat];
+		this.setSelected(coords);
+
+		if (this.onSelectCallback) {
+			this.onSelectCallback(coords);
+		}
+	}
+
+	private onMapLoadAttach(): void {
+		this.map?.off("load", this.onMapLoadAttachBound);
+		this.attachClickHandler();
+	}
+
+	private registerClickHandler(): () => void {
+		if (!this.map) {
+			return this.noop;
+		}
+
+		if (this.map.loaded()) {
+			this.attachClickHandler();
+
+			return () => {
+				this.detachClickHandler();
+			};
+		}
+
+		this.map.on("load", this.onMapLoadAttachBound);
+
+		return () => {
+			this.map?.off("load", this.onMapLoadAttachBound);
+			this.detachClickHandler();
+		};
 	}
 
 	private removeControl(id: string): void {

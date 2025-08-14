816bdac9d6b227ebced5449b2b0e4dc55d251793
timosh-yura <tron15963@gmail.com>
2025-08-14 19:26:13 +0300

    refactor(backend): renaming route types ss-268

--- packages/shared/src/modules/routes/libs/types/types.ts
+++ packages/shared/src/modules/routes/libs/types/types.ts
@@ -1,7 +1,7 @@
-export { type RoutePatchRequestDto } from "./route-update-request.dto.js";
+export { type RoutePatchRequestDto } from "./route-patch-request.dto.js";
 export { type RouteConstructRequestDto } from "./routes-construct-request-dto.type.js";
+export { type RouteCreateRequestDto } from "./routes-create-request-dto.type.js";
 export { type RouteFindAllOptions } from "./routes-find-all-options.type.js";
 export { type RouteGetAllItemResponseDto } from "./routes-get-all-item-response-dto.type.js";
-export { type RouteGetByIdResponseDto } from "./routes-get-id-response-dto.type.js";
-export { type RouteCreateRequestDto } from "./routes-request-dto.type.js";
+export { type RouteGetByIdResponseDto } from "./routes-get-by-id-response-dto.type.js";
 export { type RouteConstructResponseDto } from "./routes-response-construct-dto.type.js";

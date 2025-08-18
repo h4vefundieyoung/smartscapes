26c5795e69f7d5746b791b9f1276a60d24f38e10
Paliantsa Bohdan <49441048+h4vefundieyoung@users.noreply.github.com>
2025-08-18 15:37:09 +0300

    feat(frontend/backend/shared): implement points of interest details page ss-245 (#337)
    * feat(frontend): implement points of interest details page ss-245
    
    * feat(backend/shared): update points of interested module with description table column ss-245
    
    * feat(backend/frontend): update migrations & add responsive styling ss-245
    
    * refactor(backend/frontend): make description optional & refactor store to store 1 poi entity ss-245
    
    * docs(backend): update mermaid schema  ss-245
    
    * refactor(frontend): fix responsiveness  ss-245
    
    * refactor(frontend/backend): replace gallery with common component & update docs ss-245
    
    * feat(frontend): add loader to poi details page ss-245
    
    * refactor(frontend): solve naming issues  ss-245
    
    ---------
    
    Co-authored-by: Vladyslav Korin <114109498+vldkorin@users.noreply.github.com>

--- apps/frontend/src/pages/route-description/route-details.tsx
+++ apps/frontend/src/pages/route-description/route-details.tsx
@@ -1,4 +1,7 @@
-import { Header, Loader } from "~/libs/components/components.js";
+import image1 from "~/assets/images/route-details/placeholder-image-1.png";
+import image2 from "~/assets/images/route-details/placeholder-image-2.png";
+import image3 from "~/assets/images/route-details/placeholder-image-3.png";
+import { Header, ImageGallery, Loader } from "~/libs/components/components.js";
 import { AppRoute, DataStatus } from "~/libs/enums/enums.js";
 import {
 	useAppDispatch,
@@ -10,9 +13,8 @@ import {
 	actions as routeActions,
 	type RouteGetByIdResponseDto,
 } from "~/modules/routes/routes.js";
-import { NotFound } from "~/pages/not-found/not-found.jsx";
 
-import { ImageGallery } from "./libs/components/components.js";
+import { NotFound } from "../not-found/not-found.js";
 import styles from "./styles.module.css";
 
 const RouteDetails = (): React.JSX.Element => {
@@ -50,7 +52,7 @@ const RouteDetails = (): React.JSX.Element => {
 			/>
 			<main className={styles["container"]}>
 				<h1 className={styles["label"]}>{name}</h1>
-				<ImageGallery />
+				<ImageGallery images={[image1, image2, image3]} />
 				<p className={styles["description"]}>{description}</p>
 			</main>
 		</>

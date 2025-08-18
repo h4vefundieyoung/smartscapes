56fda1d974670f6c11f79e9d34eeae5a3b8a1163
DenisDemirov <demirov_denis@ukr.net>
2025-08-18 23:09:14 +0300

    fix(backend/shared): category create dto file type fix ss-345

--- packages/shared/src/modules/categories/libs/types/types.ts
+++ packages/shared/src/modules/categories/libs/types/types.ts
@@ -1,2 +1,2 @@
-export { type CategoryCreateRequestDto } from "./category-create-request-dto.js";
+export { type CategoryCreateRequestDto } from "./category-create-request-dto.type.js";
 export { type CategoryGetAllItemResponseDto } from "./category-get-all-item-response-dto.type.js";

5a06fb69146675d526b50146b98cbb85960f8755
Dima Bohachuk <dima.b.02@ukr.net>
2025-08-19 01:21:16 +0300

    feat(frontend): add main wrapper for manageRoutes ss-189

--- apps/frontend/src/pages/admin-dashboard/manage-routes.tsx
+++ apps/frontend/src/pages/admin-dashboard/manage-routes.tsx
@@ -2,16 +2,17 @@ import {
 	DashboardHeading,
 	PointsOfInterestTable,
 } from "./libs/components/components.js";
+import styles from "./styles.module.css";
 
 const ManageRoutes = (): React.JSX.Element => {
 	return (
-		<>
+		<main className={styles["container"]}>
 			<DashboardHeading
 				subtitle="Manage points of interest and routes."
 				title="Dashboard"
 			/>
 			<PointsOfInterestTable />
-		</>
+		</main>
 	);
 };
 

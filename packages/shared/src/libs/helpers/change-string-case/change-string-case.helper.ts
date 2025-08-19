003a5586fe8ec5c4356e65b603de23c8f281314e
DenisDemirov <demirov_denis@ukr.net>
2025-08-19 10:43:40 +0300

    fix(shared): removed unnecessary case types ss-345

--- packages/shared/src/libs/helpers/change-string-case/change-string-case.helper.ts
+++ packages/shared/src/libs/helpers/change-string-case/change-string-case.helper.ts
@@ -1,45 +1,15 @@
 import * as changeCase from "change-case";
 
-import { StringCaseType } from "../../enums/string-case-type.enum.js";
+import { StringCaseType } from "../../enums/enums.js";
+import { type ValueOf } from "../../types/value-of.type.js";
 
-const changeStringCase = (string_: string, case_: string): string => {
-	switch (case_) {
-		case StringCaseType.CAMEL: {
-			return changeCase.camelCase(string_);
-		}
-
-		case StringCaseType.CAPITAL: {
-			return changeCase.capitalCase(string_);
-		}
-
-		case StringCaseType.CONSTANT: {
-			return changeCase.constantCase(string_);
-		}
-
-		case StringCaseType.KEBAB: {
-			return changeCase.kebabCase(string_);
-		}
-
-		case StringCaseType.PASCAL: {
-			return changeCase.pascalCase(string_);
-		}
-
-		case StringCaseType.PASCAL_SNAKE: {
-			return changeCase.pascalSnakeCase(string_);
-		}
-
-		case StringCaseType.PATH: {
-			return changeCase.pathCase(string_);
-		}
-
-		case StringCaseType.TRAIN: {
-			return changeCase.trainCase(string_);
-		}
-
-		default: {
-			return string_;
-		}
-	}
+const changeStringCase = (
+	string_: string,
+	caseType: ValueOf<typeof StringCaseType>,
+): string => {
+	return caseType === StringCaseType.CAMEL
+		? changeCase.camelCase(string_)
+		: string_;
 };
 
 export { changeStringCase };

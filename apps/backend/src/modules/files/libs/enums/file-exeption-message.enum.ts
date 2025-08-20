cc1a582f5b6f8fbef764e0277ddefc7d524cfe16
Vyacheslav Khoroshev <vyacheslavkhoroshev@gmail.com>
2025-08-20 15:11:10 +0300

    fix(backend): update file size exception message ss-434

--- apps/backend/src/modules/files/libs/enums/file-exeption-message.enum.ts
+++ apps/backend/src/modules/files/libs/enums/file-exeption-message.enum.ts
@@ -1,6 +1,10 @@
+import { config } from "~/libs/modules/config/config.js";
+
+const MAX_FILE_SIZE = String(config.ENV.AWS.MAX_FILE_SIZE_MB);
+
 const FileExceptionMessage = {
 	FILE_REQUIRED: "File is required.",
-	FILE_SIZE_EXCEEDS_LIMIT: "File size exceeds the maximum limitMB.",
+	FILE_SIZE_EXCEEDS_LIMIT: `File size exceeds the maximum limit of ${MAX_FILE_SIZE}Mb.`,
 	INVALID_FILE_TYPE:
 		"Invalid file type. Only JPEG, JPG, and PNG files are allowed.",
 } as const;

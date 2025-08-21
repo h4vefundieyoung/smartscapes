a9518894b3103197f26a64e460d2d0e678bd3425
Nazarii <nazartsya@gmail.com>
2025-08-21 11:37:48 +0300

    refactor(frontend): fix point of interest section ss-390

--- apps/frontend/src/pages/route-description/libs/components/point-of-interest-section/point-of-interest-section.tsx
+++ apps/frontend/src/pages/route-description/libs/components/point-of-interest-section/point-of-interest-section.tsx
@@ -1,5 +1,5 @@
 import { PointOfInterestCard } from "~/libs/components/components.js";
-import { type RouteGetByIdResponseDto } from "~/modules/routes/libs/types/types.js";
+import { type RouteGetByIdResponseDto } from "~/modules/routes/routes.js";
 
 import styles from "./styles.module.css";
 
@@ -14,12 +14,8 @@ const PointOfInterestSection = ({
 		<section className={styles["section"]}>
 			<h2 className={styles["title"]}>Points of interest</h2>
 			<ul className={styles["cards"]}>
-				{pointOfInterests.map((pointOfInterest) => (
-					<PointOfInterestCard
-						id={pointOfInterest.id}
-						key={pointOfInterest.id}
-						name={pointOfInterest.name}
-					/>
+				{pointOfInterests.map(({ id, name }) => (
+					<PointOfInterestCard id={id} key={id} name={name} />
 				))}
 			</ul>
 		</section>

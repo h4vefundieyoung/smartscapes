f126a783343ba91d24c57fb783f3fa1fe1a4e048
whispermind <h4vefun.dieyoung@gmail.com>
2025-08-21 11:05:32 +0300

    refactor(frontend): add types ss-398

--- apps/frontend/src/modules/routes/libs/enums/route-notification.enum.ts
+++ apps/frontend/src/modules/routes/libs/enums/route-notification.enum.ts
@@ -1,5 +1,5 @@
 const RouteNotification = {
 	UPDATED: "The route has been successfully updated",
-};
+} as const;
 
 export { RouteNotification };
--- apps/frontend/src/pages/route-description/libs/constants/route-form-default-values.constant.ts
+++ apps/frontend/src/pages/route-description/libs/constants/route-form-default-values.constant.ts
@@ -1,3 +1,6 @@
-const ROUTE_FORM_DEFAULT_VALUES = { description: "", name: "" };
+const ROUTE_FORM_DEFAULT_VALUES: { description: string; name: string } = {
+	description: "",
+	name: "",
+};
 
 export { ROUTE_FORM_DEFAULT_VALUES };

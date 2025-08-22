2e939d1c96ad6a969509260a156f1f66e4375beb
vyacheslavKhoroshev <vyacheslavkhoroshev@gmail.com>
2025-08-22 14:28:53 +0300

    fix(frontend/backend): fix upload max size message and carousel wheel event ss-434 (#456)
    * fix(frontend): fix carousel wheel event ss-434
    
    * fix(backend): update file size exception message ss-434
    
    * refactor(backend/shared): rename properties according to A5 criteria ss-434
    
    ---------
    
    Co-authored-by: Dima Bohachuk <dima.b.02@ukr.net>

--- apps/backend/src/modules/files/libs/enums/file-exeption-message.enum.ts
+++ apps/backend/src/modules/files/libs/enums/file-exeption-message.enum.ts
@@ -1,8 +1,11 @@
+import { config } from "~/libs/modules/config/config.js";
+
+const MAX_FILE_SIZE = String(config.ENV.AWS.MAX_FILE_SIZE_MB);
+
 const FileExceptionMessage = {
-	FILE_REQUIRED: "File is required.",
-	FILE_SIZE_EXCEEDS_LIMIT: "File size exceeds the maximum limitMB.",
-	INVALID_FILE_TYPE:
-		"Invalid file type. Only JPEG, JPG, and PNG files are allowed.",
+	INVALID_TYPE: "Invalid file type. Only JPEG, JPG, and PNG files are allowed.",
+	REQUIRED: "File is required.",
+	SIZE_EXCEEDS_LIMIT: `File size exceeds the maximum limit of ${MAX_FILE_SIZE} Mb.`,
 } as const;
 
 export { FileExceptionMessage };

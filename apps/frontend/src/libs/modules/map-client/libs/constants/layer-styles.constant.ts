cb1bc5b5c157f1b64738dea09df02b084dee1291
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-22 15:02:34 +0200

    feat(frontend): add map layer styles constant ss-340

---
+++ apps/frontend/src/libs/modules/map-client/libs/constants/layer-styles.constant.ts
@@ -0,0 +1,14 @@
+import { getCssValue } from "~/libs/helpers/helpers.js";
+
+const MAP_LAYER_STYLES = {
+	ROUTE: {
+		LINE_COLOR: getCssValue(document.body, "--color-brand-1000"),
+		LINE_WIDTH: 6,
+	},
+	ROUTE_OUTLINE: {
+		LINE_COLOR: getCssValue(document.body, "--color-fill-press"),
+		LINE_WIDTH: 12,
+	},
+};
+
+export { MAP_LAYER_STYLES };

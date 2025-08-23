3b135f3db87c6888446a2c43019e6e9ca3aff9a1
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-23 16:52:43 +0200

    feat(frontend): add route path color, update map layer styles ss-340

--- apps/frontend/src/libs/modules/map-client/libs/constants/layer-styles.constant.ts
+++ apps/frontend/src/libs/modules/map-client/libs/constants/layer-styles.constant.ts
@@ -2,11 +2,11 @@ import { getCssValue } from "~/libs/helpers/helpers.js";
 
 const MAP_LAYER_STYLES = {
 	ROUTE: {
-		LINE_COLOR: getCssValue(document.body, "--color-brand-1000"),
+		LINE_COLOR: getCssValue(document.body, "--color-route-line"),
 		LINE_WIDTH: 6,
 	},
 	ROUTE_OUTLINE: {
-		LINE_COLOR: getCssValue(document.body, "--color-fill-press"),
+		LINE_COLOR: getCssValue(document.body, "--color-brand-1000"),
 		LINE_WIDTH: 12,
 	},
 };

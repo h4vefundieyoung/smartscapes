f29f488321fe2414d0e677e257dde1f64d2df444
Max Kotyk <mxm.kotyk@gmail.com>
2025-08-24 17:40:42 +0200

    refactor(frontend): remove getCssValue helper, update map layer styles ss-340

--- apps/frontend/src/libs/modules/map-client/libs/constants/layer-styles.constant.ts
+++ apps/frontend/src/libs/modules/map-client/libs/constants/layer-styles.constant.ts
@@ -1,12 +1,10 @@
-import { getCssValue } from "~/libs/helpers/helpers.js";
-
 const MAP_LAYER_STYLES = {
 	ROUTE: {
-		LINE_COLOR: getCssValue(document.body, "--color-route-line"),
+		LINE_COLOR: "hsl(129, 71%, 63%)",
 		LINE_WIDTH: 6,
 	},
 	ROUTE_OUTLINE: {
-		LINE_COLOR: getCssValue(document.body, "--color-brand-1000"),
+		LINE_COLOR: "hsl(162, 91%, 25%)",
 		LINE_WIDTH: 12,
 	},
 };

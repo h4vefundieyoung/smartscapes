cf983cf10ec21cae16967448f37d7330990e6adb
vldkorin <korinvlad2@gmail.com>
2025-08-24 20:10:40 +0300

    refactor(backend/shared): update patch repository and validation schema ss-521

--- apps/backend/src/modules/points-of-interest/libs/types/types.ts
+++ apps/backend/src/modules/points-of-interest/libs/types/types.ts
@@ -1,5 +1,6 @@
 export { type PointsOfInterestGetAllOptions } from "./points-of-interest-find-all-options.type.js";
 export {
+	type PointOfInterestPatchRequestDto,
 	type PointsOfInterestCreateRequestDto,
 	type PointsOfInterestGetAllItemResponseDto,
 	type PointsOfInterestGetAllQuery,

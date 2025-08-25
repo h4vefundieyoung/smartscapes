cec6dc810c654b7a164fe3536962a3982419104d
Nazarii <nazartsya@gmail.com>
2025-08-25 20:30:53 +0300

    feat(backend): add manage categories to create category ss-505

--- apps/backend/src/modules/categories/category.controller.ts
+++ apps/backend/src/modules/categories/category.controller.ts
@@ -1,4 +1,5 @@
-import { APIPath } from "~/libs/enums/enums.js";
+import { APIPath, PermissionKey } from "~/libs/enums/enums.js";
+import { checkHasPermission } from "~/libs/hooks/hooks.js";
 import {
 	type APIHandlerOptions,
 	type APIHandlerResponse,
@@ -49,6 +50,7 @@ class CategoryController extends BaseController {
 			handler: this.create.bind(this),
 			method: "POST",
 			path: CategoriesApiPath.ROOT,
+			preHandlers: [checkHasPermission(PermissionKey.MANAGE_CATEGORIES)],
 			validation: {
 				body: categoryCreateValidationSchema,
 			},

0498a1c13b46c9cff089058d498bb69cdfc65185
DenisDemirov <demirov_denis@ukr.net>
2025-08-26 10:31:19 +0300

    fix(frontend): fix merge conflict ss-496

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -20,7 +20,7 @@ import { Landing } from "./pages/landing/landing.jsx";
 import { ManageRoutes } from "./pages/manage-routes/manage-routes.js";
 import { PointsOfInterestDetails } from "./pages/points-of-interest-details/points-of-interest-details.js";
 import { Profile } from "./pages/profile/profile.js";
-import { RouteDetails } from "./pages/route-description/route-details.js";
+import { RouteDetails } from "./pages/route-details/route-details.js";
 
 pwa.register();
 

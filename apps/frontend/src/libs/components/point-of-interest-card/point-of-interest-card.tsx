2cdafb0c13f8c25a97ea9ec361f2eaacaf846d07
Vladyslav Korin <114109498+vldkorin@users.noreply.github.com>
2025-08-29 20:57:02 +0300

    feat(frontend): add maps instead of poi placeholders ss-632 (#637)

--- apps/frontend/src/libs/components/point-of-interest-card/point-of-interest-card.tsx
+++ apps/frontend/src/libs/components/point-of-interest-card/point-of-interest-card.tsx
@@ -1,19 +1,19 @@
-import defaultUrl from "~/assets/images/placeholder-card.jpg";
-import { Link } from "~/libs/components/components.js";
+import { Link, MapProvider } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { configureString } from "~/libs/helpers/helpers.js";
+import { type Coordinates } from "~/libs/types/types.js";
 
 import styles from "./styles.module.css";
 
 type Properties = {
+	coordinates: Coordinates;
 	id: number;
-	imageUrl?: string;
 	name: string;
 };
 
 const PointOfInterestCard = ({
+	coordinates,
 	id,
-	imageUrl,
 	name,
 }: Properties): React.JSX.Element => {
 	const poiDetailsLink = configureString(AppRoute.POINTS_OF_INTEREST_$ID, {
@@ -23,11 +23,13 @@ const PointOfInterestCard = ({
 	return (
 		<li className={styles["card"]}>
 			<Link className={styles["link"]} to={poiDetailsLink}>
-				<img
-					alt={name}
-					className={styles["image"]}
-					src={imageUrl ?? defaultUrl}
-				/>
+				<div className={styles["map"]}>
+					<MapProvider
+						isInteractive={false}
+						markers={[{ coordinates }]}
+						shouldFitToBounds
+					/>
+				</div>
 				<div className={styles["title"]}>{name}</div>
 			</Link>
 		</li>

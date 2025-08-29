e44712bbc912824095ed564263b478988df56b43
DenisDemirov <124807063+DenisDemirov@users.noreply.github.com>
2025-08-29 21:22:16 +0300

    feat(frontend):  landing page imrovement ss-626 (#633)
    * feat(frontend): add landing footer ss-626
    
    * feat(frontend): add popular routes load ss-626
    
    * style(frontend): fix styles ss-626
    
    * fix(frontend): removed popular from query ss-626

--- apps/frontend/src/pages/landing/libs/components/popular-section/popular-section.tsx
+++ apps/frontend/src/pages/landing/libs/components/popular-section/popular-section.tsx
@@ -1,11 +1,48 @@
+import { Loader, RouteCard } from "~/libs/components/components.js";
+import { DataStatus } from "~/libs/enums/enums.js";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useEffect,
+} from "~/libs/hooks/hooks.js";
+import { actions as routesActions } from "~/modules/routes/routes.js";
+
 import styles from "./styles.module.css";
 
 const PopularSection = (): React.JSX.Element => {
+	const dispatch = useAppDispatch();
+
+	const dataStatus = useAppSelector(({ routes }) => routes.dataStatus);
+	const routes = useAppSelector(({ routes }) => routes.data);
+
+	useEffect(() => {
+		void dispatch(routesActions.getAll({ page: 1, perPage: 10 }));
+	}, [dispatch]);
+
+	if (dataStatus === DataStatus.PENDING) {
+		return <Loader />;
+	}
+
 	return (
 		<section className={styles["section"]}>
 			<div className={styles["container"]}>
 				<h2 className={styles["title"]}>Popular routes</h2>
-				<ul className={styles["cards"]} />
+				<ul className={styles["cards"]}>
+					{routes.map(({ geometry, id, images, name, pois }) => (
+						<RouteCard
+							id={id}
+							imageUrl={images.at(0)?.url ?? null}
+							key={id}
+							mapProps={{
+								markers: pois.map((poi) => ({
+									coordinates: poi.location.coordinates,
+								})),
+								routeLine: { geometry, id: String(id) },
+							}}
+							name={name}
+						/>
+					))}
+				</ul>
 			</div>
 		</section>
 	);
